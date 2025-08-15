4949bd4e6018561ae7878fa174eeae4165a31500
Abut Seleman Tiso <abutselemant@gmail.com>
2025-08-16 00:41:24 +0300

    working on templates

--- My_Ecom/settings.py
+++ My_Ecom/settings.py
@@ -79,14 +79,16 @@ ROOT_URLCONF = 'My_Ecom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'cart.context_processors.cart',  # Include the cart context processor
+                'cart.context_processors.cart',
+
             ],
         },
     },
