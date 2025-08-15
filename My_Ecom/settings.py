c4d42b8f3cb0bc2a486b7b7eb78e8c7847b04dc8
Abut Seleman Tiso <selemantiso23@gmai.com>
2025-08-15 17:05:43 +0300

    added allowed host for railway

--- My_Ecom/settings.py
+++ My_Ecom/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = 'django-insecure-bhtwpm#@3bm8tb@w#=1%rz%kcrjqzzpi4pk9)y3_(sq5t*9jeo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['tisoecom-production.up.railway.app', 'https://tisoecom-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['tisoecom-production.up.railway.app', 'https://tisoecom-production.up.railway.app']
 
 
 # Application definition
