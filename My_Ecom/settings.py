a9893fef6ba4cc3d22bc21adaf335ba3d64893f4
Abut Seleman Tiso <abutselemant@gmail.com>
2025-08-15 18:21:39 +0300

    Initial commit

--- My_Ecom/settings.py
+++ My_Ecom/settings.py
@@ -33,11 +33,7 @@ SECRET_KEY = 'django-insecure-bhtwpm#@3bm8tb@w#=1%rz%kcrjqzzpi4pk9)y3_(sq5t*9jeo
 DEBUG = True
 
 ALLOWED_HOSTS = [' * ']
-# Correct format:
-CSRF_TRUSTED_ORIGINS = [
-    'https://tisoecom-production.up.railway.app',
-    'http://tisoecom-production.up.railway.app',  # Include if you need HTTP support
-]
+
 
 # Application definition
 
