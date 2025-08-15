dd83739ed594838312d8d94bda04a5f309d58263
Abut Seleman Tiso <abutselemant@gmail.com>
2025-08-16 00:57:01 +0300

    Fixed all template paths with my_store prefix

--- my_store/views.py
+++ my_store/views.py
@@ -25,7 +25,7 @@ def search(request):
         # Test for Null
         if not searched:
             messages.success(request, "That Product Does Not Exist..Please Try Again")
-            return render(request, "search.html", {})
+            return render(request, "my_store/search.html", {})
         else:
             return render(request, "search.html", {"searched": searched})
 
@@ -52,7 +52,7 @@ def update_password(request):
 
         else:
             form = ChangePasswordForm(current_user)
-            return render(request, "update_password.html", {"form": form})
+            return render(request, "my_store/update_password.html", {"form": form})
     else:
         messages.success(request, "You Must be logged In To view that Page..")
         return redirect("home")
@@ -68,7 +68,7 @@ def update_user(request):
             login(request, current_user)
             messages.success(request, "User Has Been Updated!!..")
             return redirect("home")
-        return render(request, "Update_user.html", {"user_form": user_form})
+        return render(request, "my_store/Update_user.html", {"user_form": user_form})
     else:
         messages.success(request, "You Must be logged In to access the page..")
         return redirect("home")
@@ -78,7 +78,7 @@ def update_user(request):
 
 def category_summary(request):
     categories = Category.objects.all()
-    return render(request, "category_summary.html", {"categories": categories})
+    return render(request, "my_store/category_summary.html", {"categories": categories})
 
 
 def category(request, foo):
@@ -90,7 +90,7 @@ def category(request, foo):
         category = Category.objects.get(name__iexact=foo)
         products = Product.objects.filter(category=category)
         return render(
-            request, "category.html", {"products": products, "category": category}
+            request, "my_store/category.html", {"products": products, "category": category}
         )
 
     except Category.DoesNotExist:
@@ -100,7 +100,7 @@ def category(request, foo):
 
 def product(request, pk):
     product = Product.objects.get(id=pk)
-    return render(request, "product.html", {"product": product})
+    return render(request, "my_store/product.html", {"product": product})
 
 
 def home(request):
@@ -108,7 +108,7 @@ def home(request):
     return render(request, "my_store/home.html", {"products": products})
 
 def about(request):
-    return render(request, "about.html", {})
+    return render(request, "my_store/about.html", {})
 
 
 def login_user(request):
@@ -138,7 +138,7 @@ def login_user(request):
             messages.error(request, "Invalid credentials. Please try again.")
             return redirect("login")
     else:
-        return render(request, "login.html", {})
+        return render(request, "my_store/login.html", {})
 
 
 def logout_user(request):
@@ -169,7 +169,7 @@ def register_user(request):
             )
             return redirect("register")
     else:
-        return render(request, "register.html", {"form": form})
+        return render(request, "my_store/register.html", {"form": form})
 
 def update_info(request):
 	if request.user.is_authenticated:
@@ -190,7 +190,7 @@ def update_info(request):
 
 			messages.success(request, "Your Info Has Been Updated!!")
 			return redirect('home')
-		return render(request, "update_info.html", {'form':form, 'shipping_form':shipping_form})
+		return render(request, "my_store/update_info.html", {'form':form, 'shipping_form':shipping_form})
 	else:
 		messages.success(request, "You Must Be Logged In To Access That Page!!")
 		return redirect('home')
