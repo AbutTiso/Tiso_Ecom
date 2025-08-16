81c8787d90b895b2805229df294c18e8f991d3ae
Abut Seleman Tiso <abutselemant@gmail.com>
2025-08-16 09:44:52 +0300

    changing H to h in home.html

--- My_Ecom/settings.py
+++ My_Ecom/settings.py
@@ -84,7 +84,7 @@ ROOT_URLCONF = 'My_Ecom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "my_store" / "templates"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
